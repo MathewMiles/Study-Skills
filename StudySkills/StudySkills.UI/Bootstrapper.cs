3e58d25a00a7e7ce74db0ab9854e8fcd8572c33f
Mathew <maxander1999@hotmail.com>
2019-12-31 22:37:59 -0500

    Injection set up, some icons added, title bar started

--- StudySkills/StudySkills.UI/Bootstrapper.cs
+++ StudySkills/StudySkills.UI/Bootstrapper.cs
@@ -11,14 +11,47 @@ namespace StudySkills.UI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _simpleContainer = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _simpleContainer.Instance(_simpleContainer);
+
+            _simpleContainer
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _simpleContainer.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _simpleContainer.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _simpleContainer.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _simpleContainer.BuildUp(instance);
         }
     }
 }
--- StudySkills/StudySkills.UI/MainViews/ShellViewModel.cs
+++ StudySkills/StudySkills.UI/MainViews/ShellViewModel.cs
@@ -7,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace StudySkills.UI.MainViews
 {
-    public class ShellViewModel : Screen
+    public class ShellViewModel : Conductor<object>
     {
+        private TitleBarViewModel _titleBarVM;
+
+        public ShellViewModel(TitleBarViewModel titleBarVM)
+        {
+            _titleBarVM = titleBarVM;
+            ActivateItem(_titleBarVM);
+        }
     }
 }
