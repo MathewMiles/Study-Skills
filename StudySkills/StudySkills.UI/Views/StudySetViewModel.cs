e780826d28e2602386ef050e1009173fd608f20e
Mathew <maxander1999@hotmail.com>
2020-01-03 01:50:54 -0500

    Added and styled ListBox for study sets

--- StudySkills/StudySkills.UI/Views/StudySetViewModel.cs
+++ StudySkills/StudySkills.UI/Views/StudySetViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using StudySkills.UI.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,27 @@ namespace StudySkills.UI.Views
     public class StudySetViewModel : Screen
     {
         private IEventAggregator _eventAggregator;
+        private List<StudySet> _studySets = new List<StudySet>();
 
         public StudySetViewModel(
             IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+            Test();
+        }
+
+        public List<StudySet> StudySets => _studySets;
+
+        private void Test()
+        {
+            for(int c=1; c<9; c++)
+            {
+                StudySets.Add(new StudySet()
+                {
+                    Name = $"Study Set {c} longbitattheendlol",
+                    Terms = 120
+                });
+            }
         }
     }
 }
