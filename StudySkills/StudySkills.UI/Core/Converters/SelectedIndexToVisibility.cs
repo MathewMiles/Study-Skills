59503487c02dde6075e8c9cc7b28dd87866a5727
Mathew <maxander1999@hotmail.com>
2020-01-14 13:13:39 -0500

    Term side appears when Study Set is selected and name changes as well

--- StudySkills/StudySkills.UI/Core/Converters/SelectedIndexToVisibility.cs
+++ StudySkills/StudySkills.UI/Core/Converters/SelectedIndexToVisibility.cs
@@ -8,15 +8,16 @@ using System.Windows.Data;
 
 namespace StudySkills.UI.Core.Converters
 {
-    public class SelectedItemToVisibility : IValueConverter
+    public class SelectedIndexToVisibility : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if(value == null)
+            if (value is int)
             {
-                return Visibility.Hidden;
+                if((int)value >= 0)
+                    return Visibility.Visible;
             }
-            return Visibility.Visible;
+            return Visibility.Hidden;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
