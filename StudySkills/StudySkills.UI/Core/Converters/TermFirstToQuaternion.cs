fdca097311d5b37dbd3006a1ccbb7d66b7331bd6
Mathew <maxander1999@hotmail.com>
2020-02-27 11:30:08 -0500

    Added option to display term or definition first

---
+++ StudySkills/StudySkills.UI/Core/Converters/TermFirstToQuaternion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Media.Media3D;
+using System.Windows.Data;
+
+namespace StudySkills.UI.Core.Converters
+{
+    public class TermFirstToQuaternion : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                if ((double)value == 0)
+                    return new Quaternion(1, 0, 0, 0);
+                else
+                    return new Quaternion(0, 0, 0, 1);
+            }
+            return new Quaternion(0, 0, 0, 1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
